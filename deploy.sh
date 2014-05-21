1181be1495c2b1c501b3a972f9a01712522bf4cc
Laurent Doguin <ldoguin@nuxeo.com>
2014-05-21 16:04:48 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 if [ ! -f ./hugo ]; then
-  wget https://github.com/spf13/hugo/releases/download/v0.10/hugo_0.10_linux_amd64.tar.gz
-  tar -xvf  hugo_0.10_linux_amd64.tar.gz
+  wget https://github.com/spf13/hugo/releases/download/v0.10/hugo_0.10_linux_amd64.tar.gz -O-| tar -xvf
   mv ./hugo_0.10_linux_amd64/hugo_0.10_linux_amd64 ./hugo
   rm -r hugo_0.10_linux_amd64/ hugo_0.10_linux_amd64.tar.gz
 fi
 ./hugo
+rm -rf /tmp/static-arken.io
 mv public /tmp/static-arken.io
-git checkout gh-pages
+git checkout -f gh-pages
 git rm --cached -r .
 git clean -fd
-git commit -m"delete preexisting site"
-cp -r  /tmp/static-arken.io/* .
+cp -r /tmp/static-arken.io/* .
 echo arken.io >> CNAME
-git add *
-git commit -m"update static site"
-git push
-git checkout master
+git add -A
+git commit -m"Build: ${BUILD_NUMBER} update static site"
+git push origin gh-pages
 rm -rf /tmp/static-arken.io
